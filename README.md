d4f503db369233b39b8bee0b229473644997f883
Hoe <lkh_bear94@hotmail.com>
2021-05-22 11:20:51 +0800

    dvc initial

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ install DVC Library
 pip install urllib3 certifi
 pip install dulwich --global-option="--pure"
 ```
+
+run the dvc code
+
+```bash
+dvc init
+dvc repro
+```
--- src/load_data.py
+++ src/load_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 @hydra.main(config_name = '../config.yaml')
 def load_data(config):
-    df = pd.read_csv(config.dataset.raw, sep = '')
+    df = pd.read_csv(config.dataset.raw, sep = ',')
     return df
 
 if __name__ == "__main__":
