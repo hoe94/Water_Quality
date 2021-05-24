0af199dc6ce3fd0bf5d24f5978c7839b68932c0f
Hoe <lkh_bear94@hotmail.com>
2021-05-24 14:41:10 +0800

    hyperparemet tuning v2

--- src/hyperparameter_tuning.py
+++ src/hyperparameter_tuning.py
@@ -38,7 +38,28 @@ def Parameter(config):
         return grid_search.best_params_["n_estimators"], grid_search.best_params_["criterion"], grid_search.best_params_["max_depth"], grid_search.best_params_["max_leaf_nodes"]
     
     elif (config.algorithm == "gradient_boosting"):
-        pass
+        start_time = time.time()
+        gb = GradientBoostingClassifier()
+        parameters = {
+                      'learning_rate': config.hyperparameter_tuning.gradient_boosting.learning_rate,
+                      'n_estimators': config.hyperparameter_tuning.gradient_boosting.n_estimators,
+                      'criterion': config.hyperparameter_tuning.gradient_boosting.criterion,
+                      'max_depth': config.hyperparameter_tuning.gradient_boosting.max_depth,
+                      'max_leaf_nodes': config.hyperparameter_tuning.gradient_boosting.max_leaf_nodes
+                    }
+        grid_search = GridSearchCV(gb, 
+                                   param_grid = parameters, 
+                                   scoring = "accuracy", 
+                                   cv = config.hyperparameter_tuning.gradient_boosting.cv,
+                                   verbose = config.hyperparameter_tuning.gradient_boosting.verbose)
+        train_x_scaled, test_x_scaled, train_y, test_y = standardization(config)
+
+        grid_search.fit(train_x_scaled, train_y)
+        #print(f"Best Parameters Setting: {grid_search.best_params_}")
+        #print(f"Best Score: {grid_search.best_score_}")
+        #print(f"running time ： {str(datetime.timedelta(seconds = (time.time() - start_time)))}")
+
+        return grid_search.best_params_["learning_rate"], grid_search.best_params_["n_estimators"], grid_search.best_params_["criterion"], grid_search.best_params_["max_depth"], grid_search.best_params_["max_leaf_nodes"]
 
 if __name__ == "__main__":
     Parameter()
--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1,19 +1,74 @@
 import pandas as pd
 import numpy as np
 import hydra
+import mlflow
+import os
+
 from standardization import standardization
 from hyperparameter_tuning import Parameter
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+import pickle
 
 @hydra.main(config_name= '../config.yaml')
 
 def train_model(config):
     train_x_scaled, test_x_scaled, train_y, test_y = standardization(config)
-    if (config.algorithm == "random_forest"):
-        n_estimators, criterion, max_depth, max_leaf_nodes = Parameter(config)
-        pass
-    elif (config.algorithm == "gradient_boosting"):
-        pass
+    #check the model exist in saved_models path
+
+    if len(os.listdir(config.model_path)) != 0:
+        if (config.algorithm == "random_forest"):
+            rf = RandomForestClassifier( n_estimators = config.parameters.random_forest.n_estimators,
+                                         criterion = config.parameters.random_forest.criterion,
+                                         max_depth = config.parameters.random_forest.max_depth,
+                                         max_leaf_nodes = config.parameters.random_forest.max_leaf_nodes
+                                        )
+            rf.fit(train_x_scaled, train_y)
+            rf_y_pred = rf.predict(test_x_scaled)
+            print(accuracy_score(test_y, rf_y_pred))
+    
+
+        elif (config.algorithm == "gradient_boosting"):
+            gb = GradientBoostingClassifier( learning_rate = config.parameters.gradient_boosting.learning_rate,
+                                             n_estimators = config.parameters.gradient_boosting.n_estimators,
+                                             criterion = config.parameters.gradient_boosting.criterion,
+                                             max_depth = config.parameters.gradient_boosting.max_depth,
+                                             max_leaf_nodes = config.parameters.gradient_boosting.max_leaf_nodes)
+
+            gb.fit(train_x_scaled, train_y)
+            gb_y_pred = gb.predict(test_x_scaled)
+            print(accuracy_score(test_y, gb_y_pred))
+
+    #Hyperparameter Tuning for selected algorithm
+    else: 
+        if (config.algorithm == "random_forest"):
+            n_estimators, criterion, max_depth, max_leaf_nodes = Parameter(config)
+            rf = RandomForestClassifier( n_estimators = n_estimators,
+                                         criterion = criterion,
+                                         max_depth = max_depth,
+                                         max_leaf_nodes = max_leaf_nodes
+                                        )
+            rf.fit(train_x_scaled, train_y)
+            rf_y_pred = rf.predict(test_x_scaled)
+            print(accuracy_score(test_y, rf_y_pred))
+            with open( os.path.join(config.model_path, 'model.pkl'), 'wb')as file:
+                pickle.dump(rf, file)
+
+        elif (config.algorithm == "gradient_boosting"):
+            learning_rate, n_estimators, criterion, max_depth, max_leaf_nodes = Parameter(config)
+            gb = GradientBoostingClassifier(
+                                            learning_rate = learning_rate,
+                                            n_estimators = n_estimators,
+                                            criterion = criterion,
+                                            max_depth = max_depth,
+                                            max_leaf_nodes = max_leaf_nodes
+                                        )
+            gb.fit(train_x_scaled, train_y)
+            gb_y_pred = gb.predict(test_x_scaled)
+            print(accuracy_score(test_y, gb_y_pred))
+            with open( os.path.join(config.model_path, 'model.pkl'), 'wb')as file:
+                pickle.dump(gb, file)
 
 if __name__ == "__main__":
     train_model()
