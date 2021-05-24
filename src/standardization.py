17878f083a9a16834264c5ca52a39b8c38a52b4a
Hoe <lkh_bear94@hotmail.com>
2021-05-24 08:59:30 +0800

    Feature Scaling

--- src/standardization.py
+++ src/standardization.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 @hydra.main(config_name = '../config.yaml')
-def feature_scaling(config):
+def standardization(config):
     
     train = pd.read_csv(config.split_data.train_path)
     test = pd.read_csv(config.split_data.test_path)
@@ -21,4 +21,4 @@ def feature_scaling(config):
     return train_x_scaled, test_x_scaled, train_y, test_y
 
 if __name__ == "__main__":
-    feature_scaling()
+    standardization()
